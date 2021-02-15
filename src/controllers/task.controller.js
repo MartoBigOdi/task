50e989307974be77614ccf0830911a507297937d
Marto <martin.vasconcelo@gmail.com>
2021-02-15 12:44:15 -0300

    Hicimos el CRUD, todo ok por ahora

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -20,6 +20,7 @@ export const createTask = async (req, res) => {
     const newTask = new Task({
         title: req.body.title,
         descripcion: req.body.descripcion,
+        done: req.body.done ? req.body.done : false //Usamos un ternario para verificar que venga un valor en la respuesta. Sino pone "false" por default
     });
     await newTask.save();//Guardamos la tarea en la DB
     console.log(newTask);
@@ -30,7 +31,7 @@ export const createTask = async (req, res) => {
 //exportamos la const que usamos para guardar el método que hicimos para busar una tarea por id
 export const findById = async (req, res) => {
     const task = await Task.findById(req.params.id);
-        try{
+        try {
             res.json(task);
         } catch {
             res.json({
@@ -55,3 +56,30 @@ export const deleteTask = async (req, res) => {
 }
 
 
+//exportamos la const que contiene el método que devuelve todas las tareas en done: true.
+export const findAllTaskDone = async (req, res) => {
+    const tasks = await Task.find({done: true}); //Consultamos ala DB y lo guardamos en una const para luego devolver al cliente.
+    try {
+        res.json(tasks);
+    } catch {
+        res.json({
+            message:'There is no Task done'
+        })
+    }
+}
+
+
+//exportamos la const que guarda el método para actualizar. "update"
+
+export const updateTask = async (req, res) => {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body) //Este método tiene dos parametros, uno que toma el id de la tarea a actualizar y otro que toma los datos del req.body para hacerlo. 
+    try {
+        res.json({
+            message: 'Updated successfully'
+        });
+    } catch {
+        res.json({
+            message:'There is no Id Task'
+        })
+    }
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -12,11 +12,16 @@ router.get('/', taskController.findAllTasks);
 //Creamos una tarea nueva con esta ruta y la guardamos
 router.post('/', taskController.createTask);
 
+//Ruta para buscar las tareas hechas "done: true" 
+router.get('/done', taskController.findAllTaskDone) //IMPORTANTE poner la ruta antes de las busquedas de ID porque nos genera un error forzado, crea un conflicto de rutas
+
 //Ruta para buscar tarea por ID
 router.get('/:id', taskController.findById)
 
 //Ruta para borrar tarea por ID
 router.delete('/:id', taskController.deleteTask)
 
+//Ruta para actualizar tarea por ID
+router.put('/:id', taskController.updateTask)
 
 export default router;
